ca8dba94002522631ad72b6bbd34b26ca75b34e3
Somjade Keswongrot <somjade.keswongrot@thomsonreuters.com>
2017-08-03 11:57:48 +0700

    Implemented tippy code

---
+++ lib/tippy/builder.rb
@@ -0,0 +1,30 @@
+module Tippy
+
+	class Builder
+		def initialize total:, gratuity:
+			@total = total
+			@gratuity = gratuity
+		end 	
+	end
+
+	def generate
+		return calculation if number_based?
+		string_based
+	end	
+
+	def number_based?
+		(@gratuity.is_a? Numeric) || (@gratuity.interger?) 
+	end	
+
+	def string_based?
+		case @gratuity.downcase
+			when 'high'	then calculation 25
+			when 'standard'	then calculation 18
+			when 'low'	then calculation 15
+		end
+	end
+
+	def calculation gratuit = @gratuity
+		@total += @total * (@gratuity.to_f / 100)
+	end	
+end
